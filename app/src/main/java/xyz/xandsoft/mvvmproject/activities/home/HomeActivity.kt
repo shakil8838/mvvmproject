9580da8c16332d9ea80485470d2fd20e777c0f9e
shakil8838 <eng.shakil8838@gmail.com>
2020-06-14 05:37:33 +0600

    drawer added

--- app/src/main/java/xyz/xandsoft/mvvmproject/activities/home/HomeActivity.kt
+++ app/src/main/java/xyz/xandsoft/mvvmproject/activities/home/HomeActivity.kt
@@ -2,11 +2,19 @@ package xyz.xandsoft.mvvmproject.activities.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_home.*
 import xyz.xandsoft.mvvmproject.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(home_toolbar)
+        val navController = Navigation.findNavController(this, R.id.home_fragment)
+        NavigationUI.setupWithNavController(home_navigation_view, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
